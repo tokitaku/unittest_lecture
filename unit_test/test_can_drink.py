ecbe14b8a1ee63dc9d7e7c70659a24489d5697fd
ikuma-hiroyuki <chemicalbondo@gmail.com>
2024-03-03 07:03:47 +0900

    順番入れ替え

--- unit_test/test_can_drink.py
+++ unit_test/test_can_drink.py
@@ -1,6 +1,31 @@
 from unittest import TestCase
 
 
+def can_drink_alcohol(age):
+    """
+    引数 age を受け取り、20以上なら '飲酒可能です' という文字列を返す
+
+    age が float の場合は TypeError を発生させる
+    age が整数でない場合は TypeError を発生させる
+    age が負の数の場合は ValueError を発生させる
+    age が20未満の場合は ValueError を発生させる
+
+    :param age: int
+    :return: str
+    """
+
+    if isinstance(age, float):
+        raise TypeError("整数を入力してください")
+    elif not isinstance(age, int):
+        raise TypeError("整数以外は判定できません")
+    elif age < 0:
+        raise ValueError("正の数を入力してください")
+    elif age < 20:
+        raise ValueError("飲酒不可です")
+
+    return "飲酒可能です"
+
+
 class TestCanDrinkAlcohol(TestCase):
     """ can_drink_alcohol() のテスト """
 
@@ -36,28 +61,3 @@ class TestCanDrinkAlcohol(TestCase):
         with self.assertRaises(ValueError) as e:
             can_drink_alcohol(19)
         self.assertEqual(str(e.exception), "飲酒不可です")
-
-
-def can_drink_alcohol(age):
-    """
-    引数 age を受け取り、20以上なら '飲酒可能です' という文字列を返す
-
-    age が float の場合は TypeError を発生させる
-    age が整数でない場合は TypeError を発生させる
-    age が負の数の場合は ValueError を発生させる
-    age が20未満の場合は ValueError を発生させる
-
-    :param age: int
-    :return: str
-    """
-
-    if isinstance(age, float):
-        raise TypeError("整数を入力してください")
-    elif not isinstance(age, int):
-        raise TypeError("整数以外は判定できません")
-    elif age < 0:
-        raise ValueError("正の数を入力してください")
-    elif age < 20:
-        raise ValueError("飲酒不可です")
-
-    return "飲酒可能です"
