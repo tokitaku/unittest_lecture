f4c53462898f4ebdb7cdb47451e7ac9624aa118d
ikuma <chemicalbondo@gmail.com>
2024-03-05 16:57:34 +0900

    微修正

--- working/worker.py
+++ working/worker.py
@@ -21,43 +21,33 @@ class Worker:
         :param is_weekday: 勤務日区分
         """
 
-        self._name = name
+        self.name = name
         self._starting_time = starting_time
         self._finishing_time = finishing_time
         self._break_time = break_time
         self._is_weekday = is_weekday
-        self._total_worktime = self._calc_total_worktime()  # 総労働時間
+        self.total_worktime = self._calc_total_worktime()  # 総労働時間
         # self.over_worktime = self._calc_overtime()  # 平日所定外時間
         # self.scheduled_worktime = self._calc_scheduled_worktime()  # 平日所定内労働時間
-        self._scheduled_worktime, self._over_worktime = self._calc_weekday_worktime()  # 平日所定内労働時間と平日所定外時間
-        self._holiday_worktime = self._total_worktime if not is_weekday else timedelta(0)  # 休日労働時間
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def total_worktime(self) -> timedelta:
-        return self._total_worktime
-
-    @property
-    def scheduled_worktime(self) -> timedelta:
-        return self._scheduled_worktime
-
-    @property
-    def over_worktime(self) -> timedelta:
-        return self._over_worktime
-
-    @property
-    def holiday_worktime(self) -> timedelta:
-        return self._holiday_worktime
+        self.scheduled_worktime, self.over_worktime = self._calc_weekday_worktime()  # 平日所定内労働時間と平日所定外時間
+        self.holiday_worktime = self.total_worktime if not is_weekday else timedelta(0)  # 休日労働時間
 
     def _calc_total_worktime(self) -> timedelta:
-        """総労働時間を計算する"""
+        """
+        総労働時間を計算する
+
+        :return: 総労働時間
+        """
+
         return self._finishing_time - self._starting_time - self._break_time
 
     # def _calc_scheduled_worktime(self) -> timedelta:
-    #     """平日所定内労働時間を計算する"""
+    #     """
+    #     平日所定内労働時間を計算する
+    #
+    #     :return: 平日所定内労働時間
+    #     """
+    #
     #     if self.is_weekday:
     #         overflow_time = self.total_worktime - SCHEDULED_WORKINGTIME
     #         if overflow_time > timedelta(0):
@@ -67,7 +57,12 @@ class Worker:
     #     return timedelta(0)
     #
     # def _calc_overtime(self):
-    #     """平日所定外時間を計算する"""
+    #     """
+    #     平日所定外時間を計算する
+    #
+    #     :return: 平日所定外時間
+    #     """
+    #
     #     if self.is_weekday:
     #         scheduled_over = self.total_worktime - SCHEDULED_WORKINGTIME
     #         if scheduled_over > timedelta(0):
@@ -77,11 +72,16 @@ class Worker:
     #     return timedelta(0)
 
     def _calc_weekday_worktime(self) -> (timedelta, timedelta):
-        """平日所定内労働時間と平日所定外時間を計算する"""
+        """
+        平日所定内労働時間と平日所定外時間を計算する
+
+        :return: 平日所定内労働時間, 平日所定外時間
+        """
+
         if self._is_weekday:
-            overflow_time = self._total_worktime - self.SCHEDULED_WORKINGTIME
+            overflow_time = self.total_worktime - self.SCHEDULED_WORKINGTIME
             if overflow_time > timedelta(0):
                 return self.SCHEDULED_WORKINGTIME, overflow_time
             else:
-                return self._total_worktime, timedelta(0)
+                return self.total_worktime, timedelta(0)
         return timedelta(0), timedelta(0)
