10caaa2fbdb2f2c41c87878341d8f4a18cedd98a
ikuma <chemicalbondo@gmail.com>
2024-03-11 17:37:21 +0900

    修正

--- tests/test_score.py
+++ tests/test_score.py
@@ -4,15 +4,9 @@ from blackjack.card import Card
 from blackjack.player import Player
 
 
-@pytest.fixture()
-def setup():
+def test_calculate_score_20():
     spade_king = Card('♠', 'K')  # 1. Card インスタンスの生成
     diamond_queen = Card('♦', 'Q')
-    yield spade_king, diamond_queen
-
-
-def test_calculate_score_20(setup):
-    spade_king, diamond_queen = setup
     user = Player("test")
     user.hand = [spade_king, diamond_queen]  # 2. 手札にカードを追加
     user.calculate_score()  # 3. スコアを計算
