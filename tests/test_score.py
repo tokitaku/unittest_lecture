abd3e87ee2cd0edaef84eb01bbd146342c682c1d
ikuma <chemicalbondo@gmail.com>
2024-03-11 17:32:12 +0900

    修正

--- tests/test_score.py
+++ tests/test_score.py
@@ -6,16 +6,14 @@ from blackjack.player import Player
 
 @pytest.fixture()
 def setup():
-    hertz_ace = Card('♥', 'A')  # 1. Card インスタンスの生成
-    spade_king = Card('♠', 'K')
-    club_9 = Card('♣', '9')
-    yield hertz_ace, spade_king, club_9
+    spade_king = Card('♠', 'K')  # 1. Card インスタンスの生成
+    diamond_queen = Card('♦', 'Q')
+    yield spade_king, diamond_queen
 
 
 def test_calculate_score_20(setup):
-    """スコアが正しく計算されているかテスト (20点)"""
-    hertz_ace, spade_king, club_9 = setup
+    spade_king, diamond_queen = setup
     user = Player("test")
-    user.hand = [hertz_ace, spade_king, club_9]  # 2. 手札にカードを追加
+    user.hand = [spade_king, diamond_queen]  # 2. 手札にカードを追加
     user.calculate_score()  # 3. スコアを計算
     assert user.score == 20
