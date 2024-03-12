c5a34747191ba0a7accd97343525ad3d177bf347
ikuma <chemicalbondo@gmail.com>
2024-03-12 15:18:47 +0900

    追加

---
+++ tests/test_card.py
@@ -0,0 +1,32 @@
+import pytest
+from blackjack.card import Card
+
+
+def test_card_score_ace():
+    """Aのスコアが11点のテスト"""
+    card = Card('♠', 'A')
+    assert card.score == 11
+
+
+class TestCardScore:
+    """カードのスコアのテスト"""
+
+    @pytest.mark.parametrize('rank, expected', [
+        ('A', 11),
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
+        ('6', 6),
+        ('7', 7),
+        ('8', 8),
+        ('9', 9),
+        ('10', 10),
+        ('J', 10),
+        ('Q', 10),
+        ('K', 10),
+    ])
+    def test_card_score(self, rank, expected):
+        """カードのスコアのテスト"""
+        card = Card('♠', rank)
+        assert card.score == expected
--- tests/test_score.py
+++ tests/test_score.py
@@ -5,9 +5,66 @@ from blackjack.player import Player
 
 
 def test_calculate_score_20():
+    """手札のスコアが20点のテスト"""
     spade_king = Card('♠', 'K')  # 1. Card インスタンスの生成
     diamond_queen = Card('♦', 'Q')
-    user = Player("test")
-    user.hand = [spade_king, diamond_queen]  # 2. 手札にカードを追加
-    user.calculate_score()  # 3. スコアを計算
-    assert user.score == 20
+    player = Player("test")
+    player.hand = [spade_king, diamond_queen]  # 2. 手札にカードを追加
+    player.calculate_score()  # 3. スコアを計算
+    assert player.score == 20
+
+
+class TestScoreByFixture:
+    """手札のスコア計算のテスト(フィクスチャを使ったテスト)"""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.spade_king = Card('♠', 'K')
+        self.diamond_queen = Card('♦', 'Q')
+        self.hearts_ace = Card('♥', 'A')
+        self.club_9 = Card('♣', '9')
+
+    def test_calculate_score_20(self):
+        """手札のスコアが20点のテスト"""
+        player = Player("test")
+        player.hand = [self.spade_king, self.diamond_queen]
+        player.calculate_score()
+        assert player.score == 20
+
+    def test_calculate_score_19(self):
+        """手札のスコアが19点のテスト"""
+        player = Player("test")
+        player.hand = [self.spade_king, self.club_9]
+        player.calculate_score()
+        assert player.score == 19
+
+    def test_calculate_natual_blackjack(self):
+        """手札がナチュラルブラックジャック(21点)のテスト"""
+        player = Player("test")
+        player.hand = [self.hearts_ace, self.spade_king]
+        player.calculate_score()
+        assert player.score == 21
+
+
+class TestScoreByMarkParametrize:
+    """手札のスコア計算のテスト(マーク付きパラメータ化を使ったテスト)"""
+
+    @pytest.mark.parametrize(
+        "hand, expected",
+        [
+            ([Card('♠', 'A'), Card('♦', 'A')], 12),
+            ([Card('♠', '2'), Card('♦', '6')], 8),
+            ([Card('♠', '3'), Card('♣', '7')], 10),
+            ([Card('♥', '4'), Card('♠', '8')], 12),
+            ([Card('♠', '5'), Card('♦', '9')], 14),
+            ([Card('♠', '10'), Card('♦', 'J')], 20),
+            ([Card('♠', 'Q'), Card('♣', 'K')], 20),
+            ([Card('♥', 'A'), Card('♠', 'K')], 21),
+        ]
+    )
+    def test_calculate_score(self, hand, expected):
+        """手札のスコア計算のテスト"""
+        player = Player("test")
+        player.hand = hand
+        player.calculate_score()
+        assert player.score == expected
