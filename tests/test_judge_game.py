8132e36b6287590438a5ba31c6e4aeb507f8de57
ikuma <chemicalbondo@gmail.com>
2024-03-13 15:52:50 +0900

    追加

---
+++ tests/test_judge_game.py
@@ -0,0 +1,63 @@
+import pytest
+
+from blackjack.main import GameManager
+from blackjack.player import UserGameState
+
+
+class TestJudgeGame:
+    """勝敗判定のテスト"""
+
+    @pytest.fixture(autouse=True)
+    def setup_game_manager(self):
+        self.manager = GameManager()
+        self.user = self.manager.user
+        self.dealer = self.manager.dealer
+
+    def test_evaluate_game_user_win(self):
+        """ユーザーが勝った場合の勝敗判定"""
+        self.user.score = 19
+        self.user.is_burst = False
+
+        self.dealer.score = 16
+        self.dealer.is_burst = False
+
+        self.manager.judge_helper._judge_game()
+
+        assert self.user.game_result == UserGameState.WIN
+
+    def test_evaluate_game_user_lose(self):
+        """ユーザーが負けた場合の勝敗判定"""
+        self.user.score = 16
+        self.user.is_burst = False
+
+        self.dealer.score = 19
+        self.dealer.is_burst = False
+
+        self.manager.judge_helper._judge_game()
+
+        assert self.user.game_result == UserGameState.LOSE
+
+    def test_evaluate_game_user_draw(self):
+        """引き分けた場合の勝敗判定"""
+        self.user.score = 16
+        self.user.is_burst = False
+
+        self.dealer.score = 16
+        self.dealer.is_burst = False
+
+        self.manager.judge_helper._judge_game()
+
+        assert self.user.game_result == UserGameState.DRAW
+
+    def test_evaluate_game_user_and_dealer_burst(self):
+        """双方がバーストした場合の勝敗判定"""
+        self.user.score = 22
+        self.user.is_burst = True
+
+        self.dealer.score = 22
+        self.dealer.is_burst = True
+
+        self.manager.judge_helper._judge_game()
+
+        assert self.user.game_result == UserGameState.LOSE
+
---
+++ unit_test_answers/test_judge_game_answer.py
@@ -0,0 +1,30 @@
+import pytest
+
+from blackjack.main import GameManager
+from blackjack.player import UserGameState
+
+
+class TestJudgeGame:
+    """勝敗判定のテスト"""
+
+    @pytest.mark.parametrize('user_score, dealer_score, expected_result', [
+        (19, 16, UserGameState.WIN),  # ユーザーが勝つ
+        (16, 19, UserGameState.LOSE),  # ディーラーが勝つ
+        (16, 16, UserGameState.DRAW),  # 引き分け
+        (22, 22, UserGameState.LOSE),  # 双方バーストでユーザーが負ける
+    ])
+    def test_evaluate_game(self, user_score, dealer_score, expected_result):
+        """勝敗判定のテスト"""
+        self.manager = GameManager()
+        self.user = self.manager.user
+        self.dealer = self.manager.dealer
+
+        self.user.score = user_score
+        self.user.is_burst = user_score > 21
+
+        self.dealer.score = dealer_score
+        self.dealer.is_burst = dealer_score > 21
+
+        self.manager.judge_helper._judge_game()
+
+        assert self.user.game_result == expected_result
