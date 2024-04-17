06c734dc32cf0085689b42d1ab8e5da9dbb6180e
ikuma-hiroyuki <chemicalbondo@gmail.com>
2024-04-17 09:56:38 +0900

    微修正

--- tests/test_dealer_hit.py
+++ tests/test_dealer_hit.py
@@ -7,7 +7,7 @@ class TestDealerHit:
     """ディーラーがヒットするかどうかのテスト"""
 
     @pytest.fixture(autouse=True)
-    def setup_game_manager(self):
+    def setup(self):
         self.manager = GameManager()
         self.user = self.manager.user
         self.dealer = self.manager.dealer
@@ -30,6 +30,24 @@ class TestDealerHit:
 
         assert self.manager.judge_helper.dealer_should_hit_card() is True
 
+    def test_dealer_should_hit_dealer_lose(self):
+        """ディーラーがユーザーに得点で負けている場合は引く"""
+        self.user.score = 20
+        self.user.is_burst = False
+        self.dealer.score = 15
+        self.dealer.is_burst = False
+
+        assert self.manager.judge_helper.dealer_should_hit_card() is True
+
+    def test_dealer_should_hit_tie(self):
+        """ディーラーとユーザーが21点未満かつ同点の場合は引く"""
+        self.user.score = 20
+        self.user.is_burst = False
+        self.dealer.score = 20
+        self.dealer.is_burst = False
+
+        assert self.manager.judge_helper.dealer_should_hit_card() is True
+
     def test_dealer_should_not_hit_dealer_burst(self):
         """ディーラーがバーストしている場合は引かない"""
         self.user.score = 20
@@ -41,9 +59,9 @@ class TestDealerHit:
 
     def test_dealer_should_not_hit_dealer_win(self):
         """ディーラーが17点以上かつユーザーに勝っている場合は引かない"""
-        self.user.score = 18
+        self.user.score = 15
         self.user.is_burst = False
-        self.dealer.score = 19
+        self.dealer.score = 20
         self.dealer.is_burst = False
 
         assert self.manager.judge_helper.dealer_should_hit_card() is False
@@ -57,7 +75,7 @@ class TestDealerHit:
 
         assert self.manager.judge_helper.dealer_should_hit_card() is False
 
-    def test_dealer_should_not_hit_user_win(self):
+    def test_dealer_should_not_hit_tie(self):
         """ユーザーが21点、ディーラーも21点の場合は引かない"""
         self.user.score = 21
         self.user.is_burst = False
@@ -66,31 +84,29 @@ class TestDealerHit:
 
         assert self.manager.judge_helper.dealer_should_hit_card() is False
 
-    def test_dealer_should_hit_tie(self):
-        """ディーラーとユーザーが21点未満かつ同点の場合は引く"""
-        self.user.score = 20
-        self.user.is_burst = False
-        self.dealer.score = 20
-        self.dealer.is_burst = False
 
-        assert self.manager.judge_helper.dealer_should_hit_card() is True
+class TestDealerHitByParametrize:
+    """ディーラーがヒットするかどうかのテスト (parametrize を使用)"""
 
-    @pytest.mark.parametrize('user_score, dealer_score, expected', [
-        # ディーラーが引くパターン
+    @pytest.mark.parametrize("user_score, dealer_score, expected", [
         (15, 16, True),  # ディーラーがユーザーに得点で勝っていても17点未満の場合は引く
         (22, 16, True),  # ユーザーがバースト (21点を超える) し、ディーラーが17点未満の場合は引く
-        # ディーラーが引かないパターン
-        (18, 19, False),  # ディーラーが17点以上かつユーザーに勝っている場合は引かない
+        (20, 15, True),  # ディーラーがユーザーに得点で負けている場合は引く
+        (20, 20, True),  # ディーラーとユーザーが21点未満かつ同点の場合は引く
+        (20, 22, False),  # ディーラーがバーストしている場合は引かない
+        (15, 20, False),  # ディーラーが17点以上かつユーザーに勝っている場合は引かない
         (22, 17, False),  # ユーザーがバーストし、ディーラーが17点以上の場合は引かない
         (21, 21, False),  # ユーザーが21点、ディーラーも21点の場合は引かない
-        (20, 20, True),  # ディーラーとユーザーが21点未満かつ同点の場合は引く
-    ])
-    def test_dealer_hit(self, user_score, dealer_score, expected):
-        """ディーラーがカードを引くべきかどうかのテスト"""
-        manager = GameManager()
-        manager.user.score = user_score
-        manager.user.is_burst = user_score > 21
-        manager.dealer.score = dealer_score
-        manager.dealer.is_burst = dealer_score > 21
+    ]
+                             )
+    def test_dealer_should_hit(self, user_score, dealer_score, expected):
+        self.manager = GameManager()
+        self.user = self.manager.user
+        self.dealer = self.manager.dealer
 
-        assert manager.judge_helper.dealer_should_hit_card() is expected
+        self.user.score = user_score
+        self.user.is_burst = user_score > 21
+        self.dealer.score = dealer_score
+        self.dealer.is_burst = dealer_score > 21
+
+        assert self.manager.judge_helper.dealer_should_hit_card() == expected
